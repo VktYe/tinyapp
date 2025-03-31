de937cab55b105adb6c9d650bc840c5667b8e3ae
VktYe <v.yehorenko@gmail.com>
2025-03-31 02:12:36 -0300

    Add GET /urls/:id route and urls_show.ejs for URL info display

--- express_server.js
+++ express_server.js
@@ -26,6 +26,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:id", (req, res) => {
+  console.log("Requested ID:", req.params.id);
+  console.log("Found longURL:", urlDatabase[req.params.id]);
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]/* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
